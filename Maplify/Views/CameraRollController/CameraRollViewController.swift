e487cb6e23f433335cb834508bbaa0ff26d7f712
Evgeniy Antonoff <antonoff.evgeniy@gmail.com>
2016-04-01 14:45:32 +0300

    Deleted comments

--- Maplify/Views/CameraRollController/CameraRollViewController.swift
+++ Maplify/Views/CameraRollController/CameraRollViewController.swift
@@ -43,7 +43,6 @@ class CameraRollViewController: UIViewController, UICollectionViewDataSource, UI
     func setup() {
         self.setupCollectionView()
         self.checkPhotoAuth()
-//        self.setupImages()
     }
     
     func setupCollectionView() {

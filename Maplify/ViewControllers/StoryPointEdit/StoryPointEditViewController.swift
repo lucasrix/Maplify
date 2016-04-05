af243c83da280fe5862877395d3a8cba2d21c4fd
jowkame <jowkame@gmail.com>
2016-04-05 16:06:53 +0300

    Added storypoint edit view controller.

---
+++ Maplify/ViewControllers/StoryPointEdit/StoryPointEditViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StoryPointEditViewController.swift
+//  Maplify
+//
+//  Created by jowkame on 05.04.16.
+//  Copyright © 2016 rubygarage. All rights reserved.
+//
+
+class StoryPointEditViewController: ViewController {
+    
+    // MARK: - view controller life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setup()
+    }
+    
+    // MARK: - setup
+    func setup() {
+        
+    }
+}

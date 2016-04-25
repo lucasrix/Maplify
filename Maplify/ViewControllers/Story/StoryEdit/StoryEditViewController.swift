c199ac311074a0900530881987c7bd7e8f0ec78a
Evgeniy Antonoff <antonoff.evgeniy@gmail.com>
2016-04-25 12:07:00 +0300

    Added populating

--- Maplify/ViewControllers/Story/StoryEdit/StoryEditViewController.swift
+++ Maplify/ViewControllers/Story/StoryEdit/StoryEditViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StoryEditViewController: ViewController {
+class StoryEditViewController: ViewController, UITextViewDelegate {
     @IBOutlet weak var storyNameLabel: UILabel!
     @IBOutlet weak var storyNameTextField: UITextField!
     @IBOutlet weak var descriptionLabel: UILabel!
@@ -31,7 +31,7 @@ class StoryEditViewController: ViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-//        self.setupContent()
+        self.populateViews()
     }
     
     // MARK: - setup
@@ -46,12 +46,38 @@ class StoryEditViewController: ViewController {
     }
     
     func setupViews() {
+        self.setupStoryNameViews()
+        self.setupStoryDescriptionViews()
+        self.setupStoryPointsView()
+    }
+    
+    func setupStoryNameViews() {
+        self.storyNameLabel.text = NSLocalizedString("Label.StoryName", comment: String())
+        self.storyNameTextField.placeholder = NSLocalizedString("Text.Placeholder.EnterBriefTitle", comment: String())
+    }
+    
+    func setupStoryDescriptionViews() {
+        self.descriptionLabel.text = NSLocalizedString("Label.Description", comment: String())
+        self.updateCharactersCountLabel((self.descriptionTextView.text?.length)!)
+        
+        self.descriptionTextView.delegate = self
         self.descriptionTextView.layer.cornerRadius = CornerRadius.defaultRadius
         self.descriptionTextView.clipsToBounds = true
         self.descriptionTextView.layer.borderWidth = kAboutFieldBorderWidth
         self.descriptionTextView.layer.borderColor = UIColor.inactiveGrey().CGColor
     }
     
+    func setupStoryPointsView() {
+        self.pointsInStoryLabel.text = NSLocalizedString("Label.PostsInThisStory", comment: String())
+        self.addPostsButton.setTitle(NSLocalizedString("Button.AddPosts", comment: String()).uppercaseString, forState: .Normal)
+    }
+    
+    func populateViews() {
+        let story = StoryManager.find(self.storyId)
+        self.storyNameTextField.text = story.title
+        self.descriptionTextView.text = story.storyDescription
+    }
+
     // MARK: - navigation bar
     override func navigationBarIsTranlucent() -> Bool {
         return false
@@ -59,5 +85,21 @@ class StoryEditViewController: ViewController {
     
     override func navigationBarColor() -> UIColor {
         return UIColor.darkGreyBlue()
+    }
+    
+    // MARK: - UITextViewDelegate
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        let resultCharactersCount = (self.descriptionTextView.text as NSString).stringByReplacingCharactersInRange(range, withString: text).length
+        if resultCharactersCount <= kDescriptionTextViewMaxCharactersCount {
+            self.updateCharactersCountLabel(resultCharactersCount)
+            return true
+        }
+        return false
+    }
+    
+    func updateCharactersCountLabel(charactersCount: Int) {
+        let substringOf = NSLocalizedString("Substring.Of", comment: String())
+        let substringChars = NSLocalizedString("Substring.Chars", comment: String())
+        self.descriptionCharsNumberLabel.text = "\(charactersCount) " + substringOf + " \(kDescriptionTextViewMaxCharactersCount) " + substringChars
     }
 }

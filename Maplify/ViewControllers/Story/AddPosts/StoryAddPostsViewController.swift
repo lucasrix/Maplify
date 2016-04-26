ec7263743655ad4c628c1febe84ee4f0e0b1292a
Evgeniy Antonoff <antonoff.evgeniy@gmail.com>
2016-04-26 16:01:28 +0300

    Added tableview populating

--- Maplify/ViewControllers/Story/AddPosts/StoryAddPostsViewController.swift
+++ Maplify/ViewControllers/Story/AddPosts/StoryAddPostsViewController.swift
@@ -6,16 +6,27 @@
 //  Copyright © 2016 rubygarage. All rights reserved.
 //
 
+import RealmSwift
 import UIKit
 
-class StoryAddPostsViewController: ViewController {
+class StoryAddPostsViewController: ViewController, StoryAddPostsDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     var storyId: Int = 0
+    var storyDataSource: CSBaseTableDataSource! = nil
+    var storyActiveModel = CSActiveModel()
     
     // MARK: - view controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setup()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.loadDataFromDB()
     }
     
     // MARK: - setup
@@ -37,8 +48,39 @@ class StoryAddPostsViewController: ViewController {
         return UIColor.darkGreyBlue()
     }
     
+    func loadDataFromDB() {
+        self.storyActiveModel.removeData()
+        let realm = try! Realm()
+        let foundedStoryPoints = Array(realm.objects(StoryPoint).filter("user.id == \(SessionManager.currentUser().id)"))
+        
+        self.storyActiveModel.addItems(foundedStoryPoints, cellIdentifier: String(StoryAddPostsTableViewCell), sectionTitle: nil, delegate: self, boundingSize: UIScreen.mainScreen().bounds.size)
+        
+        self.updateSelectedStoryPoints(foundedStoryPoints)
+        
+        self.storyDataSource = CSBaseTableDataSource(tableView: self.tableView, activeModel: self.storyActiveModel, delegate: self)
+        self.storyDataSource.reloadTable()
+    }
+    
+    func updateSelectedStoryPoints(foundedStoryPoints: [StoryPoint]) {
+        let foundedStoryPointIds = foundedStoryPoints.map({$0.id})
+        let story = StoryManager.find(self.storyId)
+        let storyPointsInStory = Converter.listToArray(story.storyPoints, type: StoryPoint.self)
+        let storyPointsInStoryIds = storyPointsInStory.map({$0.id})
+        for storyPoint in storyPointsInStoryIds {
+            if let index = foundedStoryPointIds.indexOf(storyPoint) {
+                let indexPath = NSIndexPath(forRow: index, inSection: 0)
+                self.storyActiveModel.selectModel(indexPath, selected: true)
+            }
+        }
+    }
+    
     // MARK: - navigation bar actions
     override func rightBarButtonItemDidTap() {
         // TODO:
+    }
+    
+    // MARK: - StoryAddPostsDelegate
+    func reloadData() {
+        self.storyDataSource.reloadTable()
     }
 }

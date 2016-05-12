a8ac11ce51e6b51830da00b0191637fc1ea3fd36
Evgeniy Antonoff <antonoff.evgeniy@gmail.com>
2016-05-12 15:12:12 +0300

    Added content controller setting up

--- Maplify/ViewControllers/FollowingList/ContentFollowingViewController.swift
+++ Maplify/ViewControllers/FollowingList/ContentFollowingViewController.swift
@@ -14,12 +14,54 @@ enum ShowingListOption: Int {
 }
 
 class ContentFollowingViewController: ViewController {
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var contentView: UIView!
     
     var showingListOption: ShowingListOption = ShowingListOption.Followers
-
+    
+    // MARK: - view controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setup()
+    }
+    
+    // MARK: - setup
+    func setup() {
+        self.setupSegmentControl()
+        self.setupTitle()
+    }
+    
+    func setupSegmentControl() {
+        self.segmentControl.setTitle(NSLocalizedString("Label.Followers", comment: String()).capitalizedString, forSegmentAtIndex: ShowingListOption.Followers.rawValue)
+        self.segmentControl.setTitle(NSLocalizedString("Label.Following", comment: String()).capitalizedString, forSegmentAtIndex: ShowingListOption.Following.rawValue)
+        self.segmentControl.selectedSegmentIndex = self.showingListOption.rawValue
+    }
+    
+    func setupTitle() {
+        if self.showingListOption == ShowingListOption.Followers {
+            self.title = NSLocalizedString("Label.Followers", comment: String()).capitalizedString
+        } else if self.showingListOption == ShowingListOption.Following {
+            self.title = NSLocalizedString("Label.Following", comment: String()).capitalizedString
+        }
+    }
+    
+    // MARK: - navigation bar
+    override func navigationBarIsTranlucent() -> Bool {
+        return false
+    }
+    
+    override func navigationBarColor() -> UIColor {
+        return UIColor.darkGreyBlue()
+    }
+    
+    // MARK: - actions
+    @IBAction func segmentControlChangedValue(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == ShowingListOption.Followers.rawValue {
+            self.showingListOption = ShowingListOption.Followers
+        } else if sender.selectedSegmentIndex == ShowingListOption.Following.rawValue {
+            self.showingListOption = ShowingListOption.Following
+        }
+        self.setupTitle()
     }
 }

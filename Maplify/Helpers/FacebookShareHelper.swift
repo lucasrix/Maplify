a6a01225e251415bd3660ecd4d08ea6aa7c05deb
Evgeniy Antonoff <antonoff.evgeniy@gmail.com>
2016-05-24 13:13:34 +0300

    Changed base host to production

--- Maplify/Helpers/FacebookShareHelper.swift
+++ Maplify/Helpers/FacebookShareHelper.swift
@@ -11,7 +11,7 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 import UIKit
 
-let kSharingLinkPrefix = "http://maplify.herokuapp.com/share?"
+let kSharingLinkPrefix = ConfigHepler.baseHostUrl() + "/share?"
 
 class FacebookShareHelper: NSObject {
     

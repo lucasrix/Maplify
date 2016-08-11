01021782895c9b7bfd5a6f69a2b6b62abb2f97bf
Evgeniy Antonoff <antonoff.evgeniy@gmail.com>
2016-08-11 21:33:54 +0300

    Fixed pinremote download handlers

--- Maplify/Views/PostDetailView/StoryPointInfoView.swift
+++ Maplify/Views/PostDetailView/StoryPointInfoView.swift
@@ -149,10 +149,12 @@ class StoryPointInfoView: UIView, UIScrollViewDelegate, CSBaseTableDataSourceDel
         }
         
         self.storyPointImageView.pin_setImageFromURL(attachmentUrl, placeholderImage: placeholderImage, completion: { [weak self] (result) in
-            if result.error == nil {
-                self?.colorView?.alpha = storyPoint?.kind == StoryPointKind.Photo.rawValue ? 0.0 : kMapImageDownloadCompletedAlpha
+            if SessionHelper.sharedHelper.isSessionTokenExists() {
+                if result.error == nil {
+                    self?.colorView?.alpha = storyPoint?.kind == StoryPointKind.Photo.rawValue ? 0.0 : kMapImageDownloadCompletedAlpha
+                }
+                self?.populateKindImage(storyPoint)
             }
-            self?.populateKindImage(storyPoint)
         })
     }
     
